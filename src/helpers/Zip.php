ae3713fa1df0e56b943cd2d86c3e6366d83ba25f
walkskyer <walkskyer@qq.com>
2016-09-21 14:32:44 +0800

    init

---
+++ src/helpers/Zip.php
@@ -0,0 +1,32 @@
+<?php
+namespace wkUtil\helpers;
+
+
+class Zip{
+
+    /**
+     * add file or dir to a zip file
+     * @param $path
+     * @param \ZipArchive $zip ZipArchive file
+     * @param string $root
+     */
+    public static function addFileToZip($path,  &$zip, $root = ''){
+        !$root && $root = $path;
+
+        $dirsStack = [$path];
+        while ($path = array_pop($dirsStack)) {
+            $handler = opendir($path);
+            while (($filename = readdir($handler)) !== false) {
+                if ($filename != "." && $filename != "..") {
+                    if (is_dir($path . "/" . $filename)) {
+                        array_push($dirsStack, $path . "/" . $filename);
+                    } else {
+                        $pathFilename = $path . "/" . $filename;
+                        $zip->addFile($pathFilename, str_replace($root . '/', '', $pathFilename));
+                    }
+                }
+            }
+            @closedir($path);
+        }
+    }
+}
